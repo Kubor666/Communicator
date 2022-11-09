e6a6e80b4263dfb2cab886da289d655da7d3c36f
Jakub Hebda <jakubhebda@macbook-pro-2.home>
2022-11-09 07:08:35 +0000

    Updated task manager, debugging needed

--- lib/application.ex
+++ lib/application.ex
@@ -1,11 +1,12 @@
-defmodule Application do
+defmodule Communicator.Application do
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
     children = [
-      {Task, fn -> Communicator.accept(4040) end}
+      {Task, fn -> Communicator.accept(port) end}
     ]
 
-    opts = [strategy: :one_for_one, anme: Communicator.Supervisor]
+    opts = [strategy: :one_for_one, name: Communicator.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
